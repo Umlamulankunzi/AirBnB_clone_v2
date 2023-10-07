e11d3f652a997cd5c7d8cefba8a57057e343e089
Prince D Jele <diehard@localhost.localdomain>
2023-10-07 19:05:23 +0200

    updated 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,45 +1,57 @@
-# Update package repository
-exec { 'apt-update':
-  command => 'apt update -y',
-}
-
-# Install Nginx web server
+# Install Nginx if not already installed
 package { 'nginx':
   ensure => 'installed',
 }
 
-# Allow HTTP traffic through the firewall
-exec { 'allow-http':
-  command => 'ufw allow "Nginx HTTP"',
-}
-
 # Create necessary directories for web_static deployment
-file { '/data/web_static/releases/test/':
+file { '/data':
   ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-file { '/data/web_static/shared/':
+-> file { '/data/web_static':
   ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+-> file { '/data/web_static/releases':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+-> file { '/data/web_static/shared':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
+}
+
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
 # Create index.html file with content
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>\n',
+  content => '<html>\n  <head>\n  </head>\n  <body>\n    Hello, world!\n  </body>\n</html>\n',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
 }
 
-# Create symbolic link to latest release
+# Create symbolic link to the latest release
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test/',
+  force  => true,
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
-# Set ownership of the directories to 'ubuntu' user and group
-exec { 'set-ownership':
-  command => 'chown -R ubuntu:ubuntu /data',
-}
-
-# Configure Nginx to serve /hbnb_static/ from the current release directory
+# Update Nginx configuration to serve /hbnb_static/ from the current release directory
 file_line { 'nginx-config':
   path    => '/etc/nginx/sites-available/default',
   line    => '  location /hbnb_static/ { alias /data/web_static/current/; }',
